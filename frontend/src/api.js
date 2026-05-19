166c4d6e5a51bfea9e5d491aa121dd652d02ddb2
thanujamaddika <thanujamaddika2357@gmail.com>
2026-05-19 23:06:47 +0530

    frontend ready for deployment

---
+++ frontend/src/api.js
@@ -0,0 +1,3 @@
+const API_BASE = "https://dynamic-pricing-app-1.onrender.com";
+
+export default API_BASE;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
 })
